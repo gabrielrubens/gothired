688794bd53c22cc198301d4ae189a429c64c5e21
Gabriel Rubens <gabrielrubens@gmail.com>
2025-03-11 18:25:31 +0000

    tests fixed #1

--- test/controllers/job_posts_controller_test.rb
+++ test/controllers/job_posts_controller_test.rb
@@ -1,13 +1,17 @@
 require "test_helper"
 
 class JobPostsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @job_post = job_posts(:one)
+  end
+
   test "should get index" do
     get job_posts_index_url
     assert_response :success
   end
 
   test "should get show" do
-    get job_posts_show_url
+    get job_posts_url(@job_post)
     assert_response :success
   end
 end
