e1ea9c14159411f52d8020ee6c6c050d5cfeac81
Gabriel Rubens <gabrielrubens@gmail.com>
2025-03-12 16:19:50 +0000

    fixed the tests #3

--- test/controllers/beta_signups_controller_test.rb
+++ test/controllers/beta_signups_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class BetaSignupsControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @beta_signups = beta_signups(:one)
+  end
+
   test "should get create" do
     get beta_signups_create_url
     assert_response :success
