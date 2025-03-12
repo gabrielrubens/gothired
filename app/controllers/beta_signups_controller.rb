b07ed5d55fbd54b0fc1eb1b099ac07bdd151cbc1
Gabriel Rubens <gabrielrubens@gmail.com>
2025-03-12 22:47:57 +0000

    fix tests #3

--- app/controllers/beta_signups_controller.rb
+++ app/controllers/beta_signups_controller.rb
@@ -1,8 +1,24 @@
 class BetaSignupsController < ApplicationController
   def create
-    render json: { message: "Success" }, status: :ok
-    # respond_to do |format|
-    #  format.js { render :create }
-    # end
+    
+    @beta_signup = BetaSignup.new(beta_signup_params)
+
+    if @beta_signup.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js   { render :create }
+      end
+    else
+      respond_to do |format|
+        format.html { render "pages/home", status: :unprocessable_entity }
+        format.js   { render :create }
+      end
+    end
+
+  end
+
+  private
+  def beta_signup_params
+    params.require(:beta_signup).permit(:email)
   end
 end
--- test/controllers/beta_signups_controller_test.rb
+++ test/controllers/beta_signups_controller_test.rb
@@ -5,8 +5,16 @@ class BetaSignupsControllerTest < ActionDispatch::IntegrationTest
     @beta_signups = beta_signups(:one)
   end
 
-  test "should get create" do
-    get beta_signups_create_url(@beta_signups)
+  # test "should get create" do
+  #   get beta_signups_create_url(@beta_signups)
+  #   assert_response :success
+  # end
+
+  test "should create beta signup" do
+    assert_difference('BetaSignup.count') do
+      post beta_signups_url, params: { beta_signup: { email: 'test@example.com' } }, xhr: true
+    end
     assert_response :success
   end
+
 end
