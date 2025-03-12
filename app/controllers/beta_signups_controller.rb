3058d007e3feec8f3d627b84137072ae24181588
Gabriel Rubens <gabrielrubens@gmail.com>
2025-03-12 16:11:15 +0000

    Fixed test failing #3

--- app/controllers/beta_signups_controller.rb
+++ app/controllers/beta_signups_controller.rb
@@ -1,4 +1,8 @@
 class BetaSignupsController < ApplicationController
-  def create
+  def create 
+    render json: { message: "Success" }, status: :ok
+    #respond_to do |format|
+    #  format.js { render :create }
+    #end
   end
 end
