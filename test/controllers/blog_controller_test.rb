d35624afa7602ed0081018c499e6b77251c0d936
Gabriel Rubens <gabrielrubens@gmail.com>
2025-03-20 13:42:35 +0000

    fixed the blog_post tests

--- test/controllers/blog_controller_test.rb
+++ test/controllers/blog_controller_test.rb
@@ -1,13 +1,19 @@
 require "test_helper"
 
 class BlogControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @blog_post = blog_posts(:one) # Load from fixtures
+  end
+
   test "should get index" do
     get blog_index_url
     assert_response :success
+    assert_select "h5.card-title", @blog_post.title
   end
 
   test "should get show" do
-    get blog_show_url
+    get blog_path(@blog_post.slug)
     assert_response :success
+    # assert_select "h1", @blog_post.title
   end
 end
